0898d28d95733a22cf09627c89949e69fda5ed0a
alilinykh <aililnykh@dtcc.edu>
2019-06-02 15:50:33 -0400

    dev created

--- src/main/java/rocks/zipcode/atm/bank/Bank.java
+++ src/main/java/rocks/zipcode/atm/bank/Bank.java
@@ -20,6 +20,15 @@ public class Bank {
         accounts.put(2000, new PremiumAccount(new AccountData(
                 2000, "Example 2", "example2@gmail.com", 200
         )));
+        accounts.put(3000, new BasicAccount(new AccountData(
+                3000, "Example 3", "example3@gmail.com", 1500
+        )));
+        accounts.put(4000, new PremiumAccount(new AccountData(
+                4000, "Example 4", "example4@gmail.com", 2200
+        )));
+
+
+
     }
 
     public ActionResult<AccountData> getAccountById(int id) {
